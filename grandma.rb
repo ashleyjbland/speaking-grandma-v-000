8f5124d213aa23fe8e3871cb19e789d940266be1
Ashley Bland <ashleyjbland@gmail.com>
2018-08-28 23:14:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase = "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+
   end
 end
